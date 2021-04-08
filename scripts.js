e97d6980175f8f7bf2978bd52ec134e72b9029b4
Ana Laura <ana.simoes-matos@unesp.br>
2021-04-08 19:29:03 -0300

    Tentando limpar o código

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
+  
 //variaveis globais
 let qtd=0;
 let virada=0;
 let jogadas=0;
 let acertos=0;
-const c=[2];
-const i=[2];
+const identificador=[2];
+const indice=[2];
 const array = [qtd];
 const parrots = ['bobrossparrot.gif','explodyparrot.gif','fiestaparrot.gif',
 'metalparrot.gif','revertitparrot.gif','tripletsparrot.gif','unicornparrot.gif']
@@ -23,7 +24,7 @@ function QuantasCartas(){
 function AdicionarCartas(){
     for(let i=j=0;i<qtd-1;i=i+2,j++){
         array[i]=array[i+1]=`<li class="par${i}">
-        <div class="carta abaixada" onclick="virar(this,${i})"><img src="imagens/${parrots[j]}" class="verso esconder"/><img src="imagens/front.png" class="frente" /></div>
+        <div class="carta estouAbaixada" onclick="virar(this,${i})"><img src="imagens/${parrots[j]}" class="verso esconder"/><img src="imagens/front.png" class="frente" /></div>
         </li>`;
     }
     //embaralha as cartas
@@ -43,39 +44,43 @@ function virar(carta,par){
     console.log(virada);
         jogadas++;
         if(virada<2){
-        if(carta.classList.contains("abaixada")){
+        if(carta.classList.contains("estouAbaixada")){
             
-            i[virada]=par;
-            c[virada]=carta;
-            console.log(c[virada]);
+            indice[virada]=par;
+            identificador[virada]=carta;
+            //console.log(c[virada]);
             carta.classList.add('estouVirada');
-            carta.classList.remove('abaixada');
+            carta.classList.remove('estouAbaixada');
             carta.querySelector(".frente").classList.add('esconder');
             carta.querySelector(".verso").classList.remove('esconder');
             virada++;
             
         }
         if(virada===2){
-            if(Number(i[0])===Number(i[1])){
-                console.log(i[0],i[1]);
-                acertos++;
-                /*c[0].setAttribute("onclick","#");
-                c[1].setAttribute("onclick","#");*/
-            }
-            else {
-            setTimeout(desvirar,1000,c[0]);
-            setTimeout(desvirar,1000,c[1]);
-            //setTimeout('desvirar(c[1])', 1000);
-            }
-            virada=0;
+        ehIgual();
+        virada=0;
         }
     }
 fim();
 }
 
+function ehIgual(){
+    if(Number(indice[0])===Number(indice[1])){
+        //console.log(i[0],i[1]);
+        acertos++;
+        /*c[0].setAttribute("onclick","#");
+        c[1].setAttribute("onclick","#");*/
+    }
+    else {
+    setTimeout(desvirar,1000,identificador[0]);
+    setTimeout(desvirar,1000,identificador[1]);
+    //setTimeout('desvirar(c[1])', 1000);
+    }
+}
+
 function desvirar(carta){
     carta.classList.remove('estouVirada');
-    carta.classList.add('abaixada');
+    carta.classList.add('estouAbaixada');
     carta.querySelector(".frente").classList.remove('esconder');
     carta.querySelector(".verso").classList.add('esconder');
 
@@ -85,4 +90,3 @@ function fim(){
     console.log("acertos:"+acertos);
     if (acertos===(qtd/2)) alert(`Você ganhou em ${jogadas} jogadas!`);
 }
-
